e81265f7973b43a330dd6ae6db44464050ff00f6
s12901 <s12901@pjwstk.edu.pl>
2022-03-26 17:02:23 +0100

    package refactor

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var path = require("path");
 module.exports = {
-    entry: "./src/index.js",
+    entry: "./lab2/src/index.js",
     output: {path: path.join(__dirname, "dist", "assets"), filename: "bundle.js"},
     module: {rules: [{test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}]}
 };
